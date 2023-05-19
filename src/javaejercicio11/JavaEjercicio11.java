d5244e9b3fc68c672d7acc8d78983977024c8930
Kerling-A-Cando <133818998+Kerling-A-Cando@users.noreply.github.com>
2023-05-18 20:35:09 -0500

    Primer commiPrimer commiPrimer committt

---
+++ src/javaejercicio11/JavaEjercicio11.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaejercicio11;
+import java.util.Scanner;
+/**
+ *
+ * @author kcand
+ */
+public class JavaEjercicio11 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner leer = new Scanner(System.in);
+       int flag=0;
+       String resultado;
+       String cadena="";
+       do{
+           System.out.println("Ingrese una frase que termine en punto final.");
+           cadena = leer.nextLine();
+           int longitud=cadena.length();
+           //System.out.println(longitud);
+           char letra;
+           letra = cadena.charAt(longitud-1);
+           //System.out.println(letra);
+           if (letra=='.'){
+               flag=1;
+           } else {
+           }
+       }while(flag==0);
+       resultado = codificar(cadena);
+       System.out.println(resultado);
+    }
+    
+    public static String codificar(String cadena){
+        int longitud=cadena.length();
+        String cadenaResultado="";
+        char letra;
+        for(int i=0;i<longitud;i++){
+            letra = cadena.charAt(i);
+                switch (letra){
+                    case 'a':
+                        cadenaResultado=cadenaResultado + '@';
+                        continue;
+                    case 'e':
+                        cadenaResultado=cadenaResultado + '#';
+                        continue;
+                    case 'i':
+                        cadenaResultado=cadenaResultado + '$';
+                        continue;
+                    case 'o':
+                        cadenaResultado=cadenaResultado + '%';
+                        continue;
+                    case 'u':
+                        cadenaResultado=cadenaResultado + '*';
+                        continue;
+                    default:
+                        cadenaResultado=cadenaResultado+letra;
+                }
+            }
+        return cadenaResultado;
+        }
+        
+}
